af1365b5eccd40086fce003535eb3b95702ff58f
VictoriaWerberg <victoria.werberg@gmail.com>
2022-12-17 11:19:56 +0200

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,7 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL ='/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
